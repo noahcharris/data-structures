bed86ea5d060cafedf5be73aaea665f5b5822d32
Hack Reactor <hackreactor@Hacks-Mac-mini-3.local>
2013-11-13 15:58:06 -0800

    Completed prototypal.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,36 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+
+
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.nextUp = 0;
+
+  // Implement the methods below
+
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.nextUp] = value;
+    this.nextUp++;
+  },
+  dequeue: function(){
+	if (this.nextUp > 0) {
+      var result = this.storage[0];
+      this.nextUp--;
+      for (var i=0;i<this.nextUp;i++) {
+        this.storage[i] = this.storage[i+1];
+      }
+      return result;
+    } else {
+      return;
+    }
+  },
+  size: function(){
+  	return this.nextUp;
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,30 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push:function(value){
+      this.storage[this.length] = value;
+      this.length++;
+    },
+    pop:function() {
+      if (this.length > 0) {
+        var result = this.storage[this.length-1];
+        delete this.storage[this.length-1];
+        this.length--;
+        return result;
+      }
+    },
+    size:function() {
+    	return this.length;
+    }
+
+};
