6685fad948676fa53c572a4b42b9e6fbc8ccf0e7
Hack Reactor Students <noah.christopher.harris@gmail.com>
2013-11-14 18:50:00 -0800

    Finished extra credit Set stuff.

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -12,20 +12,30 @@ describe("set", function() {
   });
 
   it("add should add a value to the set", function(){
-    set.add(4);
-    set.add(5);
-    set.add(6);
-    var values = [];
-    for(var key in set._storage){
-      values.push(key);
-    }
-    expect(set.contains(4)).toBe(true);
-    expect(values).toEqual(['4','5','6']);
+    set.add("hello");
+    set.add("bee");
+    set.add("gees");
+    expect(set.contains("hello")).toBe(true);
+    expect(set.contains("bee")).toBe(true);
+    expect(set.contains("gees")).toBe(true);
   });
 
   it("should remove values from the set", function() {
     set.add(4);
     set.remove(4);
     expect(set.contains(4)).toBe(false);
+  });
+
+  it("should accept multiple data types", function() {
+    set.add([]);
+    set.add({});
+    set.add(null);
+    set.add(42);
+    set.add("Hello set world");
+    expect(set.contains([])).toBe(true);
+    expect(set.contains({})).toBe(true);
+    expect(set.contains(null)).toBe(true);
+    expect(set.contains(42)).toBe(true);
+    expect(set.contains("Hello set world")).toBe(true);
   });
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,16 +7,18 @@ var makeSet = function(){
 var setPrototype = {
   add: function(val){
     this._storage = this._storage || {};
-    this._storage[val] = true;
+    this._storage[JSON.stringify(val)] = true;
   },
   contains: function(val){
-    if (this._storage[val]){
+    var s = JSON.stringify(val);
+    if (this._storage[s]){
       return true;
     }
     return false;
   },
   remove: function(val){
-    if(this._storage[val])
+    var s = JSON.stringify(val);
+    if(this._storage[s])
       delete this._storage[val];
   }
 };
