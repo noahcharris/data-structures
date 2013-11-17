6e83cebad41f859efeaa5082253873e19bf8b5ac
Daniel Chao <daniel.h.chao@gmail.com>
2013-11-17 09:36:58 -0800

    extended bTree functionality to include traversal, removal and allValues methods

--- sprint-two/src/bTree.js
+++ sprint-two/src/bTree.js
@@ -1,8 +1,12 @@
+//This constructor will return a b-tree of order 3
+
 var makeBTree = function(){
-  //This constructor will return a B-tree of order 3
+
   var newBTree = {};
 
-  // class functions
+      ////////////////////
+      // CLASS FUNCTION //
+      ////////////////////
 
   var findClosest = function(val, node){
     if (!node.left && !node.middle && !node.right) {
@@ -20,14 +24,21 @@ var makeBTree = function(){
 
   };
 
-  // newBTree instance functions
+      ////////////////////////
+      // INSTANCE FUNCTIONS //
+      ////////////////////////
 
   newBTree.insert = function(val){
-    if(!this.root){
+
+    // if there is no root, the root simmply becomes the node. otherwise, find the closest
+    // node and perform an addValue on it
+    if (typeof val !== 'number' ){
+      throw new Error('need a numeric argument')
+    }
+    if (!this.root) {
       this.root = newBTree.makeNode(val);
     } else {
-      closestNode = findClosest(val, this.root);
-      closestNode.addValue(val);
+      findClosest(val, this.root).addValue(val);
     }
   };
 
@@ -41,7 +52,11 @@ var makeBTree = function(){
       parent  : null,
       addValue: function(val, childNode){
 
-        var node  = this;
+        var
+          node        = this,
+          grandParent = node.parent;
+
+        // since the function is recursive, a node is made upon first invocation
         childNode = childNode || newBTree.makeNode(val);
 
         // if the length is less than two, we can guarantee that the first value is filled
@@ -50,7 +65,7 @@ var makeBTree = function(){
 
         // if our node's value is less than the first value of the node we're adding
         // to, attach it to node.left
-          if(val < node.values[0]) {
+          if (val < node.values[0]) {
             node.left        = childNode;
             childNode.parent = node;
           }
@@ -63,15 +78,17 @@ var makeBTree = function(){
             node.middle      = childNode.left;
             node.right       = childNode.middle;
             childNode.parent = node;
-            if( childNode.left ){ childNode.left.parent = node; }
-            if( childNode.middle ){ childNode.middle.parent = node; }
+
+            if (childNode.left) { childNode.left.parent = node; }
+            if (childNode.middle) { childNode.middle.parent = node; }
+
           }
 
         }
 
         // if the node has been saturated, we will need to discard the node, and attach a new
         // node split along the median, and add the median to the parent node (which will split
-        // yet again if that becomes)
+        // yet again if that becomes saturated)
         else {
 
           var
@@ -81,7 +98,7 @@ var makeBTree = function(){
             median      = arr[1],
             newParent   = newBTree.makeNode(median),
             leftChild   = newBTree.makeNode(arr[0]),
-            middleChild = newBTree.makeNode(arr[2]);
+            middleChild = childNode || newBTree.makeNode(arr[2]);
 
           if (node.left) { leftChild.left = node.left; }
           if (node.middle) { leftChild.middle = node.middle; }
@@ -92,10 +109,10 @@ var makeBTree = function(){
           middleChild.parent = newParent;
 
 
-        // if there is a grandParent node, then add the node we just created to the grandParent
-        // if there is no grandParent, "parent" becomes the top of the tree
-          if(node.parent){
-            node.parent.addValue(median, newParent);
+        // if there is a "grandParent" node, then add the node we just created to the
+        // "grandParent". otherwise, "newParent" becomes the top of the tree
+          if (grandParent) {
+            grandParent.addValue(median, newParent);
           } else {
             self.root = newParent;
           }
@@ -103,5 +120,51 @@ var makeBTree = function(){
       }
     };
   };
+
+  newBTree.traverse = function(callback, node){
+    node = node || this.root;
+    callback(node);
+    if(node.left){
+      newBTree.traverse(callback, node.left);
+    }
+    if(node.middle){
+      newBTree.traverse(callback, node.middle);
+    }
+    if(node.right){
+      newBTree.traverse(callback, node.right);
+    }
+  };
+
+  newBTree.remove = function(target){
+    var i, indexOfTarget, allValues = this.allValues();
+
+    if (typeof target !== 'number' ){
+      throw new Error('need a numeric argument')
+    }
+
+    // if the target isn't in the allValues array, do nothing. otherwise reset the root
+    // and reinsert all remaining values back into the tree in order
+    indexOfTarget = allValues.indexOf(target);
+    if(indexOfTarget < 0){ return; }
+
+    this.root = null;
+    allValues.splice(indexOfTarget, 1);
+    for (i = 0; i < allValues.length; i++) {
+      this.insert(allValues[i]);
+    }
+    return target;
+  };
+
+  newBTree.allValues = function(){
+    var collection = [];
+    this.traverse(function(node){
+      for (var i = 0; i < node.values.length; i++) {
+        collection.push(node.values[i]);
+      }
+    })
+    return collection.sort(function(a, b){ return a - b; });
+  }
+
+
   return newBTree;
 };
