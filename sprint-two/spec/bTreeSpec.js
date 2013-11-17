f10202f7839c234b91a40e6e134764124fa2823c
Hack Reactor Students <noah.christopher.harris@gmail.com>
2013-11-16 16:04:49 -0800

    Finished B-Tree

--- sprint-two/spec/bTreeSpec.js
+++ sprint-two/spec/bTreeSpec.js
@@ -11,15 +11,42 @@ describe("bTree", function(){
   });
 
   it("should have a left, middle, right, and parent property", function(){
-    expect(bTree.left === undefined).toBe(false);
-    expect(bTree.middle === undefined).toBe(false);
-    expect(bTree.right === undefined).toBe(false);
-    expect(bTree.parent === undefined).toBe(false);
-    expect(bTree.values === undefined).toBe(false);
+    bTree.insert(10);
+    expect(bTree.root.left   === undefined).toBe(false);
+    expect(bTree.root.middle === undefined).toBe(false);
+    expect(bTree.root.right  === undefined).toBe(false);
+    expect(bTree.root.parent === undefined).toBe(false);
+    expect(bTree.root.values === undefined).toBe(false);
   });
 
-  it("should have an addValue function", function(){
-    expect(bTree.addValue).toEqual(jasmine.any(Function));
+  it("should have an insert function", function(){
+    expect(bTree.insert).toEqual(jasmine.any(Function));
+  });
+
+  it("should have a basic insert pattern", function(){
+    bTree.insert(9);
+    bTree.insert(5);
+    bTree.insert(17);
+    expect(bTree.root.values).toEqual([9, 17]);
+    expect(bTree.root.left.values).toEqual([5]);
+  });
+
+  it("should resplit a node if it has been saturated and a new value needs to be inserted", function(){
+    bTree.insert(15);
+    bTree.insert(30);
+    bTree.insert(20);
+    expect(bTree.root.values).toEqual([20]);
+  });
+
+  it("should handle a more complicated case", function(){
+    bTree.insert(1);
+    bTree.insert(2);
+    bTree.insert(3);
+    bTree.insert(4);
+    bTree.insert(5);
+    bTree.insert(6);
+    bTree.insert(7);
+    expect(bTree.root.values).toEqual([4]);
   });
 
 });
--- sprint-two/src/bTree.js
+++ sprint-two/src/bTree.js
@@ -1,50 +1,107 @@
-var makeBTree = function(val){
+var makeBTree = function(){
   //This constructor will return a B-tree of order 3
   var newBTree = {};
-  newBTree.values = [val];
-  newBTree.left = null;
-  newBTree.middle = null;
-  newBTree.right = null;
-  newBTree.parent = null;
 
-  newBTree.addValue = function(val){
-    var parent = this.parent;
-    if (this.values.length < 2) {
-      val > this.values[0] ? this.values.push(val) : this.values.unshift(val);
+  // class functions
+
+  var findClosest = function(val, node){
+    if (!node.left && !node.middle && !node.right) {
+      return node;
+    }
+    else if (val < node.values[0]) {
+      return node.left ? findClosest(val, node.left) : node;
+    }
+    else if (val > node.values[1]) {
+      return node.right ? findClosest(val, node.right) : node;
+    }
+    else {
+      return node.middle ? findClosest(val, node.middle) : node;
+    }
+
+  };
+
+  // newBTree instance functions
+
+  newBTree.insert = function(val){
+    if(!this.root){
+      this.root = newBTree.makeNode(val);
     } else {
-      var arr = this.values.slice(0);
-      arr.push(val);
-      arr.sort(function(a, b) { return a - b; });
-      var
-        leftChild = makeBTree(arr[0]),
-        middleChild = makeBTree(arr[2]),
-        newParent = makeBTree(arr[1]);
-
-      newParent.left = leftChild;
-      newParent.middle = middleChild;
-    }
-
-  };
-
-  newBTree.findClosest = function(val, node){
-    node = node || this;
-    if(this.values.length < 2){
-      return node;
-    }
-    if(!this.values.right && !this.values.middle && !this.values.left){
-      return node;
-    }
-    if(this.values[0] > val){
-      return findClosest(this.left, node);
-    }
-    if(this.values[0] < val && this.values[1] > val){
-      return findClosest(this.middle, node);
-    }
-    if(this.values[1] > val){
-      return findClosest(this.right, node);
+      closestNode = findClosest(val, this.root);
+      closestNode.addValue(val);
     }
   };
 
+  newBTree.makeNode = function(val){
+    var self = this;
+    return {
+      values  : [val],
+      left    : null,
+      middle  : null,
+      right   : null,
+      parent  : null,
+      addValue: function(val, childNode){
 
+        var node  = this;
+        childNode = childNode || newBTree.makeNode(val);
+
+        // if the length is less than two, we can guarantee that the first value is filled
+        // and the second is empty
+        if (node.values.length < 2) {
+
+        // if our node's value is less than the first value of the node we're adding
+        // to, attach it to node.left
+          if(val < node.values[0]) {
+            node.left        = childNode;
+            childNode.parent = node;
+          }
+
+        // if our node's value is greater than the first value of the node we're adding
+        // to, add our node's value to that array, and assign the middle and right properties
+        // to our node's left and middle properties
+          else {
+            node.values      = node.values.concat(val);
+            node.middle      = childNode.left;
+            node.right       = childNode.middle;
+            childNode.parent = node;
+            if( childNode.left ){ childNode.left.parent = node; }
+            if( childNode.middle ){ childNode.middle.parent = node; }
+          }
+
+        }
+
+        // if the node has been saturated, we will need to discard the node, and attach a new
+        // node split along the median, and add the median to the parent node (which will split
+        // yet again if that becomes)
+        else {
+
+          var
+            arr         = node.values.slice(0).concat(val).sort(function(a, b) {
+                            return a - b;
+                          }),
+            median      = arr[1],
+            newParent  = newBTree.makeNode(median),
+            leftChild   = newBTree.makeNode(arr[0]),
+            middleChild = newBTree.makeNode(arr[2]);
+
+          if (node.left) { leftChild.left = node.left; }
+          if (node.middle) { leftChild.middle = node.middle; }
+
+          newParent.left    = leftChild;
+          newParent.middle  = middleChild;
+          leftChild.parent   = newParent;
+          middleChild.parent = newParent;
+
+
+        // if there is a grandParent node, then add the node we just created to the grandParent
+        // if there is no grandParent, "parent" becomes the top of the tree
+          if(node.parent){
+            node.parent.addValue(median, newParent);
+          } else {
+            self.root = newParent;
+          }
+        }
+      }
+    };
+  };
   return newBTree;
 };
